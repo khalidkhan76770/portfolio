8693f03d1e1b544135672a67a529fa15b68cb66f
khalidkhan76770 <63289496+khalidkhan76770@users.noreply.github.com>
2025-09-14 16:37:49 +0530

    Add WhatsApp contact link and floating button

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 // pages/_app.js
 import '../styles/globals.css';
+import WhatsAppButton from '@/components/WhatsAppButton';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <WhatsAppButton />
+    </>
+  );
 }
